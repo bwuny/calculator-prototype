2d154940fd4217f0056008df6154bcb797760691
bwuny <korph88@gmail.com>
2023-10-26 18:30:13 -0300

    actually works now

--- CalcV7.py
+++ CalcV7.py
@@ -16,36 +16,30 @@ def Percentage(num1, num2):
         return "the second number must be greater than 0."
 def Average(num1, num2, num3):
     return (num1 + num2 + num3) / 3
-          
-operations = [
-    (1, Addition),
-    (2, Subtraction),
-    (3, Multiplication),
-    (4, Division),
-    (5, Percentage),
-    (6, Average)]
 
-for key, func in operations:
+operations = [Addition,Subtraction,Multiplication,Division,Percentage,Average]
+for key, func in enumerate(operations,start=1):
    print(f"{key}: {func.__name__}")
 
 while True:    
-    
-    choice = float(input("choose an operation (1-6): "))
-    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))  
-    
-    if choice in operations:
-        num1 = float(input("Enter the first number: "))
-        num2 = float(input("Enter the second number: "))
-        if choice == '6':
-            num3 = float(input("Enter the third number: "))
-            Result = operations[choice](num1, num2, num3) 
-        if choice == '5':
-            print(f"Result: {Result}%")
-    else:
-        print("operation invalid. choose a valid operation (1-6).")
+    try:
+        choice = int(input("choose an operation (1-6) "))
+        if 1<= choice <= 6:
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
+            if choice == 6:
+                num3 = float(input("Enter the third number: "))
+                Result = operations[choice-1](num1,num2,num3)
+            else:
+                Result = operations[choice-1](num1,num2)
+            print(f"Result: {Result}")
+        else:
+            print("Operation invalid. Choose a valid operation (1-6).")
+            if choice == 5:
+                print(f"Result: {Result}%") # type: ignore
+    except Exception as e:
+        print("An error occurred:", e)   
     another_calculation = str(input('another calculation? (yes ou no): ').lower())
-    if another_calculation != 'yes':
+    if another_calculation == 'no':
         print('Bye!')
         break
